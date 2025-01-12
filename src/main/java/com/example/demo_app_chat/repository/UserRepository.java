6dc8a849d4bcdbb2eeede7e60c84c12c9fbf31a8
SV-Thuc2003 <21130557@st.hcmuaf.edu.vn>
2025-01-12 21:06:15 +0700

    up code(12/1/2025) API fetch user

--- src/main/java/com/example/demo_app_chat/repository/UserRepository.java
+++ src/main/java/com/example/demo_app_chat/repository/UserRepository.java
@@ -1,28 +1,29 @@
 package com.example.demo_app_chat.repository;
 
 import com.example.demo_app_chat.model.User;
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Integer> {
+public interface UserRepository extends MongoRepository<User, String> {
     User findByUsernameAndPassword(String username, String password);
 
-    Optional<Object> findById(String id);
-
+//    Optional<Object> findById(String id);
+    Optional<User> findById(String id);
     User findByUsername(String userName);
 
     User findByEmail(String email);
 
     List<User> findByStatus(int status);
-//    @Query("SELECT u FROM User u WHERE u.id != :userId AND u.id NOT IN (SELECT f.id FROM User u2 JOIN u2.friends f WHERE u2.id = :userId)")
-//    List<User> findSuggestedFriends(@Param("userId") String userId);
+//    @Query("{ '_id': { $ne: :#{#userId} }, 'followers': { $ne: :#{#userId} } }")
+//    List<User> findSuggestedFriends(@Param("userId")String userId);
     @Query("SELECT u FROM User u LEFT JOIN u.followers f ON f.id = :userId WHERE u.id != :userId AND f.id IS NULL")
     List<User> findSuggestedFriends(@Param("userId") String userId);
-
 }
+
